acba8968ab0d88ac9b8e02df6cc1b65fb35ac5be
Koffi-Roland <krolandaziawor@gmail.com>
2020-09-07 13:33:07 +0000

    Commit-add base configuration-07.09.2020

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\View\View;
+
+class DefaultController  extends AbstractFOSRestController
+{
+    /**
+     * @Rest\Get("/accueil")
+     */
+    public function home()
+    {
+
+        $data['message'] = 'Bienvenue sur la plateforme nanududu!';
+        $data['version'] = '1.0';
+
+        return $data;
+    }
+}
